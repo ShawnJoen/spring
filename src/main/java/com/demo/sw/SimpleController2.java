54dbfc8053cafba001494e7214bb9dc49107b9de
quanchunlindellpc <quanchunlindellpc@163.com>
2017-01-27 18:19:34 +0800

    MockMvc 单元测试

--- src/main/java/com/demo/sw/SimpleController2.java
+++ src/main/java/com/demo/sw/SimpleController2.java
@@ -1,5 +1,7 @@
 package com.demo.sw;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,14 @@ import com.demo.product.ProductVO;
 
 @RestController
 public class SimpleController2 {
+	
+	private static final Logger logger = LoggerFactory.getLogger(SimpleController2.class);
+	
 	@RequestMapping("/doJSON2")
 	public ProductVO doJSON2() {
+		
+		logger.info("doJSON2 start -------");
+		
 		ProductVO vo = new ProductVO("샘풀상품", 3000);
 		return vo;
 	}
